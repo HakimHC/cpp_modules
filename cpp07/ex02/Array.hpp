2af9ee41e99a1af029ada9b4e3283932f497f54f
HakimHC <ahmedh90111@gmail.com>
2023-07-24 01:08:57 +0200

    Finished. Added additional tests, fixed copy assignment operator

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: hakahmed <hakahmed@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 05:52:14 by hakahmed          #+#    #+#             */
-/*   Updated: 2023/07/06 23:38:01 by hakahmed         ###   ########.fr       */
+/*   Updated: 2023/07/24 01:06:26 by hakahmed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ class Array {
 		unsigned int 	_size;
 
 	public:
-		Array() {this->_arr = new T[0]; this->_size = 0;}
+		Array() 	{this->_arr = new T[0]; this->_size = 0;}
+		~Array() 	{delete[] this->_arr;}
 
 		Array(unsigned int n) {
 			this->_arr = new T[n];
@@ -35,27 +36,26 @@ class Array {
 			}
 			this->_size = n;
 		}
-		Array(Array& other) {
+		Array(const Array& other) {
 			this->_arr = new T[other._size];
 			for (size_t i = 0; i < other._size; i++) {
 				this->_arr[i] = other._arr[i];
 			}
 			this->_size = other._size;
 		}
-		Array& operator=(Array& rhs) {
-			delete this->_arr;
+		Array& operator=(const Array& rhs) {
+			delete[] this->_arr;
 			this->_arr = new T[rhs._size];
 			for (size_t i = 0; i < rhs._size; i++) {
 				this->_arr[i] = rhs._arr[i];
 			}
 			this->_size = rhs._size;
+      return *this;
 		};
-		~Array() {}
 
 		T& operator[](int i) {
 			if (i < 0 || (unsigned int) i >= this->_size) {
-				/* std::cerr << "Index out of bounds" << std::endl; */
-				throw std::exception();
+				throw std::runtime_error("fatal: index out of bounds");
 				return this->_arr[0];
 			}
 			return this->_arr[i];
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,18 +6,40 @@ int main() {
 	Array<int> a(5);
 
 	try {
+		std::cout << "============ LETS TRY TO RANDOMLY ACCESS OUR ARRAY AT INDEX -432 ================" << std::endl;
 		std::cout << a[-432] << std::endl;
 	}
 	catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
+	try {
+		Array<int> b;
+		std::cout << "============ NOW LETS CREATE AN EMPTY ARRAY, AND LETS ACCESS IT AT INDEX 0 ================" << std::endl;
+		std::cout << b[0] << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 
+	std::cout << "============ NOW, LETS CREATE A VALID ARRAY, SIZE 5, AND POPULATE IT ================" << std::endl;
 	a[0] = 1;
 	a[1] = 2;
 	a[2] = 3;
 	a[3] = 4;
 	a[4] = 5;
-	for (size_t i = 0; i < a.size(); i++)
-		std::cout << a[i] << std::endl;
+	for (size_t i = 0; i < a.size(); i++) std::cout << a[i] << std::endl;
+
+	std::cout << "============ FINALLY, LETS CHECK IF IT IS PERFORMING DEEP COPY ================" << std::endl;
+	std::cout << "Array<int> a2;" << std::endl;
+	std::cout << "a2 = a;" << std::endl;
+	std::cout << "a[0] = 127;" << std::endl;
+  Array<int> a2;
+  a2 = a;
+  a[0] = 127; // change a2 first element to 127
+	std::cout << "A ELEMENTS: " << std::endl;
+	for (size_t i = 0; i < a.size(); i++) std::cout << a[i] << std::endl;
+
+	std::cout << "A2 ELEMENTS: " << std::endl;
+  for (size_t i = 0; i < a2.size(); i++) std::cout << a2[i] << std::endl;
 	return 0;
 }
